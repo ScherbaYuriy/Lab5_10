1020824b89ecc8fbe2468d1d1723a6ddcf05162c
ScherbaYuriy <sherba6639@gmail.com>
2022-04-25 20:19:47 +0300

    test
    test

---
+++ Lab5_10/.idea/Lab_1.java
@@ -0,0 +1,26 @@
+public class Lab_1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 500; i <= 650; i = i + 10) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
+
+
+// Варіант while
+//        int i = 500;
+//        while (i <= 650) {
+//            System.out.println(i + " ");
+//            i = i + 10;
+//        }
+
+
+// Варіант do - while
+//        int i = 500;
+//        do {
+//            System.out.println(i);
+//            i = i + 10;
+//        } while (i <= 650);
---
+++ Lab5_10/src/Lab_3.java
@@ -0,0 +1,13 @@
+
+public class Lab_3 {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        for (int a = 1; a <= 10; a++) {
+            if (i % a == 0) {
+                System.out.println(a);
+            }
+        }
+    }
+}
